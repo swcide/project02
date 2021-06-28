5a61f74733c8f71fa235ef09daaf5e6592e8cce0
swcide <swcide@gmail.com>
2021-06-28 17:33:51 +0900

    'hanghaelog_20210628'

---
+++ src/main/java/com/project02/hanghaelog/HanghaeLogApplication.java
@@ -0,0 +1,13 @@
+package com.project02.hanghaelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project02/hanghaelog/HanghaeLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project02.hanghaelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
